5b4ec064a1304d8767330999de88515f9a6ad040
kei <pythonkei@gmail.com>
2024-12-24 18:02:40 +0800

    Step8 search

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from .models import Listing # This is Database name Listing *uppcase
+from django.shortcuts import render, get_object_or_404
+from .models import Listing # This is Database name Listing *upcase
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from listings.choices import price_choices, bedroom_choices, district_choices
+
 # Use for seprate page show more record in database
 
 # Create your views here.
@@ -14,16 +16,24 @@ def index(request):
     page = request.GET.get('page') # page variable decele, ('page') is parameter for html call url ?page={{i}}
     paged_listings = paginator.get_page(page) # Paginator function for seperate page
     context = {
-        'listings' : paged_listings
+        'listings' : paged_listings,
     }
     # This line mean : "variable" : "value", context variable
     # Use python DICT function method packageing all record to template html
     return render(request,'listings/listings.html', context)
 
-def listing(request, listing_id): 
+def listing(request, listing_id):
     # listing_id active while initial here
     # function from urls.py: path('<int:listing_id>', views.listing, name='listing')
-    return render(request,'listings/listing.html')
+    listing = get_object_or_404(Listing, pk=listing_id)
+    context = {'listing' : listing } #Check record exsiting or not, if no record go to 404 page
+    return render(request,'listings/listing.html', context)
+
 
 def search(request):
-    return render(request,'listings/search.html')
+    context = {
+        'price_choices':price_choices,
+        'bedroom_choices':bedroom_choices,
+        'district_choices':district_choices,
+    }
+    return render(request,'listings/search.html', context)
--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from listings.models import Listing
+from realtors.models import Realtor
+from listings.choices import price_choices, bedroom_choices, district_choices
 # Create your views here.
 
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
-    context = { 'listings' : listings }
+    context = {
+        'listings' : listings,
+        'price_choices':price_choices,
+        'bedroom_choices':bedroom_choices,
+        'district_choices':district_choices,
+    }
     return render(request,'pages/index.html', context)
 
 def about(request):
-    return render(request,'pages/about.html')
+    realtors = Realtor.objects.order_by('-hire_date') # order_by -hire _date mean recent date
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True) # Enable mvp realtors and active by CMS
+    context = {  # This mean: context as package list variable with sub 2 variable for check
+        'realtors' :realtors,
+        'mvp_realtors' : mvp_realtors
+    }
+    return render(request,'pages/about.html', context) # put context variable into about.html
+
+def search(request):
+    context = {
+        'price_choices':price_choices,
+        'bedroom_choices':bedroom_choices,
+        'district_choices':district_choices,
+    }
+    return render(request,'listings/search.html', context)
